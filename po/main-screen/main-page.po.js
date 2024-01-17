dda8a74a80f5a2a05bf62a008f68a7db97be1909
Oleh Buchnev <o.buchnev@conundrum.ai>
2024-01-18 01:53:52 +0300

    Polish the tests and add reporters

--- po/main-screen/main-page.po.js
+++ po/main-screen/main-page.po.js
@@ -1,39 +1,13 @@
 import Page from './base.po.js'
-import { remote } from 'webdriverio';
-
-
-/* const createDriver = () => {
-    const capabilities = {
-      platformName: 'iOS',
-      'appium:automationName': 'XCUITest',
-      'appium:deviceName': 'iPhone 14 Pro',
-      'appium:appPackage': '/Users/olegbuchnev/Library/Developer/Xcode/DerivedData/YouTube-erervxmenhqfpafyfbcokjkrknuk/Build/Products/Debug-iphonesimulator/YouTube.app'
-    };
-  
-    const wdOpts = {
-      hostname: process.env.APPIUM_HOST || 'localhost',
-      port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
-      logLevel: 'info',
-      capabilities,
-    };
-    
-    
-    return remote(wdOpts);
-  }; */
-
     
 class MainPage extends Page {
-    //get mainHeaderText () {return $('//XCUIElementTypeStaticText[@name=\"Swift Radio\"]')}
-    //get mainHeaderText () {return driver.$('//XCUIElementTypeStaticText[@name=\"Swift Radio\"]')}
-
-    async open () {
-        await super.open('login')
-    }
-
-    async submit () {
-        await this.submitBtn.click()
-    }
-
+    get mainHeaderText () {return $('//XCUIElementTypeStaticText[@name="Swift Radio"]')};
+    get hamburgerMenu () {return $('~icon hamburger')};
+    get closePopUpButton () {return $('~btn close')};
+    get nowPlaytingButton () {return $('~btn nowPlaying')};
+    get logoIcon () {return $('~logo')};
+    get emailMenuItem () {return $('//XCUIElementTypeStaticText[@name="email me"]')};
+    get counldNotSendEmailError () {return $(`//XCUIElementTypeStaticText[@name="Could Not Send Email"]`)};
 }
 
-export {MainPage}
+export default new MainPage();
